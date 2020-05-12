e0e1f752134ccd4f0df128a06b00b9e7689eed0e
Josh Souza <development@codethat.rocks>
2020-05-12 00:15:34 -0700

    Improve storage, make it json

--- petfeeder/store.py
+++ petfeeder/store.py
@@ -1,6 +1,7 @@
 from petfeeder.events import Meal, HealthCheck
+import jsonpickle
 
-from logging import error, debug
+from logging import error, debug, info
 import pickle
 import os
 
@@ -47,8 +48,9 @@ class Store(object):
                 self.data[key] = value
 
     def save(self):
-        with open(self._config_file, 'wb') as file:
-            pickle.dump(self.data, file)
+        with open(self._config_file, 'w') as file:
+            encoded_pickle = jsonpickle.encode(self.data, unpicklable=True)
+            file.write(encoded_pickle)
 
     def set(self, key, value):
         self.data[key] = value
@@ -59,11 +61,28 @@ class Store(object):
         self.save()
 
     def load(self):
+        loaded = False
         try:
-            with open(self._config_file, 'rb') as file:
-                self.data = pickle.load(file)
-            self.ensure_defaults()
-            self.loaded = True
+            with open(self._config_file, 'r') as file:
+                self.data = jsonpickle.decode(file.read(), keys=True)
+            loaded = True
         except Exception as e:
-            self.ensure_defaults()
-            error("Error loading config file %s. Setting defaults." % str(e))
+            info("Failed to load new style pickle: %s" % str(e))
+        if not loaded:
+            try:
+                with open(self._config_file, 'rb') as file:
+                    old_data = pickle.load(file)
+                # Convert format to the new one
+                encoded_pickle = jsonpickle.encode(old_data, unpicklable=True)
+                self.data = jsonpickle.decode(encoded_pickle, reset=False)
+
+                loaded = True
+                self.save()  # Ensure we save in the updated format
+            except Exception as e:
+                info("Failed to load old style pickle: %s" % str(e))
+        self.ensure_defaults()
+        if not loaded:
+            error("Error loading config file %s. Setting defaults." %
+                  self._config_file)
+            self.save()  # Ensure we create the default file
+        self.loaded = True
